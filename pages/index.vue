d3f31b3f5a6159cd7595206814ac76734c81cc77
Madyan Eka <madyan.eka95@gmail.com>
2018-10-06 06:20:22 +0700

    get started Sekilas pandang tentang Nuxt.js

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,6 @@
 <template>
   <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        my-first-nuxt-app
-      </h1>
-      <h2 class="subtitle">
-        My super-excellent Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
+    <h1>Hello world!</h1>
   </section>
 </template>
 
