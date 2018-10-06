cc994e3fad57e163e055fa73684a7c13816a6c15
Madyan Eka <madyan.eka95@gmail.com>
2018-10-06 08:02:30 +0700

    creating nested routes

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,23 +1,3 @@
 <template>
-  <div>
-    <input
-      v-model="userId"
-      type="text">
-    <button @click="onLoadUser">Load User</button>
-  </div>
+  <p>Please enter an id and click the button.</p>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      userId: ''
-    }
-  },
-  methods: {
-    onLoadUser() {
-      this.$router.push(`/users/${ this.userId }`)
-    }
-  }
-}
-</script>
