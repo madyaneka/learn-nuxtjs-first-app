ad414928543487e3012bd1753c6b081f4370f1a4
Madyan Eka <madyan.eka95@gmail.com>
2018-10-06 11:22:14 +0700

    add header to error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>An error occured, we're sorry!</h1>
+  <div>
+    <Header/>
+    <h1>An error occured, we're sorry!</h1>
+  </div>
 </template>
+
+<script>
+import Header from '@/components/Header'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
