ae558c2b3e9059421ee491ee691055465e03b615
Madyan Eka <madyan.eka95@gmail.com>
2018-10-06 07:52:36 +0700

    validating parameters

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -1,3 +1,11 @@
 <template>
   <h1>A single user, with ID: {{ $route.params.id }}</h1>
 </template>
+
+<script>
+export default {
+  validate (data) {  // this method will be executed before rendering a route (nuxt.js method)
+    return /^\d+$/.test(data.params.id)
+  }
+}
+</script>
